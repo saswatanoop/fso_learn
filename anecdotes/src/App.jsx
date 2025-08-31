72878154b9140b9e6b4671abb2938b78468e7d95
sswt19 <saswatanoop@gmail.com>
2025-08-31 15:47:41 +0530

    Exercise 1.11 to 1.14

---
+++ anecdotes/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState, useMemo } from 'react'
+
+const MostVotes = ({ anecdote, votes }) => {
+  return <>
+    <h1>Anecdote with most votes</h1>
+    <p>{anecdote}</p>
+    <p>has {votes} votes</p>
+  </>
+}
+
+const DisplayAnecdote = ({ anecdote, votes, handleNextAnecdote, handleVote }) => {
+  return <>
+    <h1>Anecdote of the day</h1>
+    <p>{anecdote}</p>
+    <p>has {votes} votes</p>
+
+    <button onClick={handleNextAnecdote}>next anecdote</button>
+    <button onClick={handleVote}>vote</button>
+  </>
+}
+const getRandomInt = (a, b) => {
+  return Math.floor(Math.random() * (b - a + 1)) + a;
+};
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+
+  const mostVotes = useMemo(() => {
+    let index = 0;
+    for (let i = 0; i < votes.length; i++) {
+      if (votes[i] > votes[index]) index = i;
+    }
+    return index;
+  }, [votes]);
+
+  const handleClick = () => {
+    setSelected(getRandomInt(0, anecdotes.length - 1))
+  }
+
+  const handleVote = () => {
+    setVotes(prev => {
+      const newVotes = [...prev]
+      newVotes[selected] += 1
+      return newVotes
+    })
+  }
+
+  return (
+    <div>
+      <DisplayAnecdote anecdote={anecdotes[selected]} votes={votes[selected]} handleNextAnecdote={handleClick} handleVote={handleVote} />
+      <MostVotes anecdote={anecdotes[mostVotes]} votes={votes[mostVotes]} />
+    </div>
+  )
+}
+
+export default App
