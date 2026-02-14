1dcfbe9d8c8786ff8219653182f567948320f765
sswt19 <saswatanoop@gmail.com>
2026-02-14 12:24:43 +0530

    Exercise 3.1 to 3.6

---
+++ backend/phonebook_backend/index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+app.get("/api/persons", (req, res) => {
+    res.json(persons);
+})
+app.get("/api/persons/:id", (req, res) => {
+    const id = req.params.id;
+    const person = persons.find(p => p.id === id);
+    if (person) {
+        res.json(person);
+    }
+    else {
+        res.status(404).end();
+    }
+})
+app.post("/api/persons", (req, res) => {
+    body = req.body;
+    if (!body.name || !body.number) {
+        res.status(400).json({ error: "name or number is missing" });
+    }
+    else {
+        const exists = persons.some(p => p.name.toLowerCase() === body.name.toLowerCase());
+        if (exists) {
+            res.status(400).json({ error: "name must be unique" });
+        }
+        else {
+            const newPerson = {
+                id: String(Math.max(...persons.map(p => Number(p.id))) + 1),
+                name: body.name,
+                number: body.number
+            }
+            persons.push(newPerson);
+            res.json(newPerson);
+        }
+    }
+})
+
+app.delete("/api/persons/:id", (req, res) => {
+    const id = req.params.id;
+    persons = persons.filter(p => p.id !== id);
+    res.status(204).end();
+})
+
+app.get("/info", (req, res) => {
+    const info = `Phonebook has info for ${persons.length} people <br/> ${new Date()}`;
+    res.send(info);
+})
+
+app.listen(3001, () => {
+    console.log("Server running on port 3001");
+})  
