70bc4d0b3f8cc30f2d41ea2d544ba8d64bcd3668
sswt19 <saswatanoop@gmail.com>
2026-03-05 00:49:52 +0530

    Exercise 4.6

--- backend/blog_backend/utils/list_helper.js
+++ backend/blog_backend/utils/list_helper.js
@@ -16,8 +16,33 @@ const favoriteBlog = (blogs) => {
   return blogs.reduce(reducer)
 }
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null
+
+  const counts = {}
+
+  blogs.forEach((blog) => {
+    counts[blog.author] = (counts[blog.author] || 0) + 1
+  })
+
+  let topAuthor = null
+  let maxBlogs = 0
+
+  for (const author in counts) {
+    if (counts[author] > maxBlogs) {
+      maxBlogs = counts[author]
+      topAuthor = author
+    }
+  }
+
+  return {
+    author: topAuthor,
+    blogs: maxBlogs
+  }
+}
 
 module.exports = {
   totalLikes,
   favoriteBlog,
+  mostBlogs
 }
