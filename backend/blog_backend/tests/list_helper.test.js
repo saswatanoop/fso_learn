c6769eb480cc9be0a873b8be4bf4828645319934
sswt19 <saswatanoop@gmail.com>
2026-03-05 00:43:06 +0530

    Exercise 4.5

--- backend/blog_backend/tests/list_helper.test.js
+++ backend/blog_backend/tests/list_helper.test.js
@@ -2,26 +2,27 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5,
+    __v: 0
+  }
+]
+const biggerList = listWithOneBlog.concat([
+  {
+    _id: '5a422aa71b54a676234d17f9',
+    title: 'Another Blog',
+    author: 'John Doe',
+    url: 'https://example.com/blog2',
+    likes: 10,
+  },
+])
+
 describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-      likes: 5,
-      __v: 0
-    }
-  ]
-  const biggerList = listWithOneBlog.concat([
-    {
-      _id: '5a422aa71b54a676234d17f9',
-      title: 'Another Blog',
-      author: 'John Doe',
-      url: 'https://example.com/blog2',
-      likes: 10,
-    },
-  ])
 
   test('of empty list is zero', () => {
     const result = listHelper.totalLikes([])
@@ -36,4 +37,22 @@ describe('total likes', () => {
     const result = listHelper.totalLikes(biggerList)
     assert.strictEqual(result, 15)
   })
+})
+
+describe('favorite blog', () => {
+
+  test('of empty list is null', () => {
+    const result = listHelper.favoriteBlog([])
+    assert.deepStrictEqual(result, null)
+  })
+
+  test('when list has only one blog, equals that blog', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    assert.deepStrictEqual(result, listWithOneBlog[0])
+  })
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.favoriteBlog(biggerList)
+    assert.deepStrictEqual(result, biggerList[1])
+  })
+
 })
--- backend/blog_backend/utils/list_helper.js
+++ backend/blog_backend/utils/list_helper.js
@@ -6,6 +6,18 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  const reducer = (favorite, blog) => {
+    return blog.likes > favorite.likes ? blog : favorite
+  }
+  // will take the blogs[0] as the initial value of favorite and then compare it with the rest of the blogs
+  return blogs.reduce(reducer)
+}
+
+
 module.exports = {
-  totalLikes
+  totalLikes,
+  favoriteBlog,
 }
