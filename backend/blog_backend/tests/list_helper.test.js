59b69c94f91e3f0e8a8efd217d9c34b430bd1420
sswt19 <saswatanoop@gmail.com>
2026-03-05 00:28:35 +0530

    Exercise 4.4

--- backend/blog_backend/tests/list_helper.test.js
+++ backend/blog_backend/tests/list_helper.test.js
@@ -2,9 +2,38 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+  const biggerList = listWithOneBlog.concat([
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: 'Another Blog',
+      author: 'John Doe',
+      url: 'https://example.com/blog2',
+      likes: 10,
+    },
+  ])
 
-  const result = listHelper.dummy(blogs)
-  assert.strictEqual(result, 1)
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(biggerList)
+    assert.strictEqual(result, 15)
+  })
 })
--- backend/blog_backend/utils/list_helper.js
+++ backend/blog_backend/utils/list_helper.js
@@ -1,7 +1,11 @@
-const dummy = (_) => {
-  return 1
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+
+  return blogs.reduce(reducer, 0)
 }
 
 module.exports = {
-  dummy
+  totalLikes
 }
